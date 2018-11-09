1d7575a327e634b8609fd20d5bf088ebb1cc531e
Administrateur <Administrateur@UTILISA-EEOHTA2>
2018-11-09 14:09:39 +0100

    Ajoutez des fichiers projet.

---
+++ exo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            try
+            {
+                StudentListisNotFoundException ret = new StudentListisNotFoundException();
+                throw ret;
+            }
+            catch(StudentListisNotFoundException stuEx)
+            {
+                Console.WriteLine(stuEx.Message.ToString());
+
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ exo/StudentListisNotFoundException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exo
+{
+    class StudentListisNotFoundException : Exception
+    {
+        public StudentListisNotFoundException()
+        {
+            throw new StudentListisNotFoundException("");
+        }
+
+        public StudentListisNotFoundException(string message)
+
+            :base("List student not found")
+        {
+
+
+        }
+
+        public StudentListisNotFoundException(string message, Exception inner)
+            : base(message, inner)
+        {
+
+        }
+    }
+}
